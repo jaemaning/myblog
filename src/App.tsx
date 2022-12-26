2dbc2d6f90179bcaed5053f455782deaa3245dd5
김재만 <lovesay00@naver.com>
2022-12-26 19:32:27 +0900

    20221226_kjm(react blog project 세팅)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h4 style={{ textAlign: 'center', marginLeft: "auto", marginRight: "auto" }}>Jeam 블로그</h4>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<div>홈페이지</div>}></Route>
+        <Route path="/works" element={<div>작업물페이지</div>}></Route>
+      </Routes>
+
+
+      <div className='footer'>
+        <button className='footer-btn left-bottom'>1</button>
+        <button className='footer-btn right-bottom'>2</button>
+      </div>
     </div>
   );
 }
